90f886c54a250b4d746f9b2014dc6b341f2795b6
kimki0605 <rlarlgus2800@naver.com>
2026-08-11 17:43:58 +0900

    0811commit

---
+++ TestProject/src/Test.java
@@ -0,0 +1,20 @@
+class A{
+	
+}
+
+class B{
+	
+}
+
+public class Test {
+
+	public static void main(String[] args) {
+		//주석 프로그램에 대한 설명
+		//실행에 어떠한 영향도 없음
+		/*여러줄 주석
+		*/
+		//블록주석 설정 : ctrl + shift + /
+		//블록주석 해제 : ctrl + shift + \
+		System.out.println("test 입니다.");
+	}
+}

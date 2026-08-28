046750d8b19643524ca2e51d2b8295c54a508473
kimki0605 <rlarlgus2800@naver.com>
2026-08-28 16:07:20 +0900

    0828 commit

---
+++ JavaWorkspace/Servlet01/src/main/java/forward/ServletRefreshFirst.java
@@ -0,0 +1,28 @@
+package forward;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first02")
+public class ServletRefreshFirst extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * refresh를 통한 포워딩
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf8");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<script type='text/javascript'>");
+		out.print("location.href='sec03';");
+		out.print("</script>");
+	}
+
+}

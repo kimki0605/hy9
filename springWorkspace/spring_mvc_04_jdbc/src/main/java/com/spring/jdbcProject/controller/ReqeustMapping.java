123e8be7edce98bc5e6d22484dd175f92c8ef445
kimki0605 <rlarlgus2800@naver.com>
2026-09-14 17:53:12 +0900

    0914 commit

---
+++ springWorkspace/spring_mvc_04_jdbc/src/main/java/com/spring/jdbcProject/controller/ReqeustMapping.java
@@ -0,0 +1,5 @@
+package com.spring.jdbcProject.controller;
+
+public @interface ReqeustMapping {
+
+}
---
+++ springWorkspace/spring_mvc_05_mybatis/src/main/java/com/spring_mvc/mybatis/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.spring_mvc.mybatis.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring_mvc.mybatis.dto.ProductDTO;
+import com.spring_mvc.mybatis.service.ProductService;
+
+@Controller
+public class ProductController {
+	@Autowired
+	ProductService service;
+	
+	@RequestMapping("/")
+	public String viewIndex() {
+		return "index";
+	}
+	
+	@RequestMapping("/product/listAllproduct")
+	public String listAllProduct(Model model) {
+		ArrayList<ProductDTO> prdList = service.listAllProduct();
+		return "product/productListView";
+	}
+}
